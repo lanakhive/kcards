53c633ca50cc8eba8d318db339215d0a435c3896
Lanak <lanak@lanakhive.com>
2013-11-22 22:22:43 +0100

    Optimize pile manager using single canvas

--- pilemanager.lua
+++ pilemanager.lua
@@ -11,12 +11,15 @@ function PileManager.create(x,y)
 	pm.scl = 0.20
 	pm.x = x
 	pm.y = y
+	pm.c = love.graphics.newCanvas(1024,1024)
+	pm.cxo = 0
 
 	setmetatable(pm,PileManager)
 	return pm
 end
 
 function PileManager:draw()
+	love.graphics.draw(self.c,self.cxo)
 	for i,j in ipairs(self.pile) do
 		local cardface = cardGet(j.num, j.suit)
 		love.graphics.draw(cardface,j.x,j.y,j.r,j.s,j.s,512,512)
@@ -25,6 +28,7 @@ end
 
 function PileManager:clear()
 	self.pile = {}
+	self.c:clear()
 end
 
 function PileManager:addCard(num,suit,x,y,r,s)
@@ -40,30 +44,27 @@ function PileManager:addCard(num,suit,x,y,r,s)
 		tr = math.rad(math.random(360)) - math.rad(180),
 		ts = self.scl
 	})
-	local maxpile = 10
-	if #self.pile > maxpile then
-		for i=1,#self.pile-maxpile,1 do
-			rem = self.pile[i]
-			rem.tx = self.x * global.ws
-			rem.ty = self.y * global.hs
-			rem.ts = 0.15
-			rem.rem = true
-		end
-	end
 end
 
 function PileManager:update(dt)
+	if global.w <= 1024 then self.cxo = 0
+	else self.cxo = (global.w - 1024) / 2 end
 	if dt > 1 then dt = 0 end
 	for i = #self.pile,1,-1 do
 		j = self.pile[i]
 		local speed = 4
-		if j.rem then speed = 1 end
 		j.x = lerp(j.x, j.tx, speed*dt)
 		j.y = lerp(j.y, j.ty, speed*dt)
 		j.r = lerp(j.r, j.tr, speed*dt)
 		j.s = lerp(j.s, j.ts, speed*dt)
-		if j.rem  and math.abs(rem.x - self.x) < 5 and math.abs(rem.y - self.y) < 5 then
+
+		if math.abs(j.x-j.tx) < 0.01 and math.abs(j.y-j.ty) < 0.01 and math.abs(j.r-j.tr) < 0.01 and math.abs(j.s-j.ts) < 0.01 and i == 1 then
+			love.graphics.setCanvas(self.c)
+			local cardface = cardGet(j.num, j.suit)
+			love.graphics.draw(cardface,j.x-self.cxo,j.y,j.r,j.s,j.s,512,512)
+			love.graphics.setCanvas()
 			table.remove(self.pile, i)
+
 		end
 	end
 end
